bfd3a3a5e2e5a7ae738fb12e75afc8de123d989f
Slava <110696118+sl7one@users.noreply.github.com>
2023-03-19 14:21:06 +0200

    Updates

--- src/components/Icon/Icon.jsx
+++
@@ -1,9 +0,0 @@
-import icons from '../../img/icons.svg';
-
-export const Icon = () => {
-  return (
-    <svg width={52} height={56} fill={'orange'}>
-      <use href={icons + '#icon-star'}></use>
-    </svg>
-  );
-};
