814125313ae54c848f31fbb3a7fb4bc002d24712
Slava <110696118+sl7one@users.noreply.github.com>
2023-03-19 13:21:54 +0200

    upd

--- src/index.js
+++ src/index.js
@@ -7,9 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-  <BrowserRouter
-  // basename="/genesistest"
-  >
+  <BrowserRouter basename="/genesistest">
     <App />
   </BrowserRouter>
 
